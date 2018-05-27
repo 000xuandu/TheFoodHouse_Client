ec2ab54b4f5322acb381ba29c17c5c372425fdf2
The Dark <cntt.lexuandu@gmail.com>
2018-05-27 14:10:05 +0700

    Finish Show Details For Food (Update)

--- app/src/main/java/thedark/example/com/thefoodhouse_client/Food/FoodDetailsActivity.java
+++ app/src/main/java/thedark/example/com/thefoodhouse_client/Food/FoodDetailsActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,10 +32,22 @@ public class FoodDetailsActivity extends AppCompatActivity {
     DatabaseReference foods;
     private String FoodId = "";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_details);
+
+        // toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         //Firebase:
         database = FirebaseDatabase.getInstance();
@@ -78,5 +92,15 @@ public class FoodDetailsActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
